2872e60429305def64cadfe59923fb55922becd2
baylorjep <baylorjeppsen@gmail.com>
2024-08-14 11:12:32 -0600

    success screen now lets users know to check their email

--- src/components/SuccessSignup.js
+++ src/components/SuccessSignup.js
@@ -5,8 +5,8 @@ function SuccessSignup() {
     return (
         <div className="container px-5 d-flex align-items-center justify-content-center">
             <div className="col-lg-6 text-center">
-                <h1>Success! Account Created</h1>
-                <p>Your account has been created successfully.</p>
+                <h1>Success! Account Created.</h1>
+                <p>Check your Email to confirm your Account.</p>
                 <Link to="/signin" className="btn btn-secondary btn-lg">Click Here to Sign In</Link>
             </div>
         </div>
