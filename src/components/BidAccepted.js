d487db93d6e32114a3e18dcd84de6d44b017f43f
baylorjep <baylorjeppsen@gmail.com>
2024-08-15 11:21:55 -0600

    bidaccept

--- src/components/BidAccepted.js
+++ src/components/BidAccepted.js
@@ -7,7 +7,7 @@ function BidSuccess() {
             <div className="col-lg-6 text-center">
                 <br/>
                 <h2>Bid Approved!</h2>
-                <p>You have approved the job. We will notify the business.</p>
+                <p>You have approved the job. The business will be reaching out to you soon!</p>
                 <Link className="btn btn-secondary" to="/my-bids">Back to Bids</Link>
             </div>
         </div>
