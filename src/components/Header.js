d4a3fc7ab4bd73a34bf4982e47cae638a6dea784
baylorjep <baylorjeppsen@gmail.com>
2024-08-21 17:44:10 -0600

    rotating services

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,27 @@
 import React from 'react';
+import RotatingText from './RotatingText';
 import '../App.css';
 import { Link } from 'react-router-dom';
 
 function Header() {
-//search stuff below, restore this when ready to use search
-    //const searchInput = document.getElementById('search');
-
-    //const search = () => {
-        //const searchValue = searchInput.value;
-        //console.log(`Searched: ${searchValue}`);
-    //}
-
     return (
         <header className="masthead-index">
             <div className="container px-5">
                 <div className="row gx-5 justify-content-center">
-                    <div className="col-lg-6 text-center">
+                    <div className="col-lg-8 text-center">
                         <div className="text-container">
-                            <h1>Real-Time Bids on Your Projects</h1>
+                            <h1>
+                                Real-Time Bids on Your Next <RotatingText />
+                            </h1>
                             <p>
                                 Our revolutionary bidding system simplifies obtaining real quotes for the services you need, allowing for easy comparison all in one place.
                             </p>
                             <div className="search-container">
-                            <Link to="Signup">
-                                <button className="search-button">
-                                Get Started
-                                </button>
-                            </Link>
+                                <Link to="Signup">
+                                    <button className="search-button">
+                                        Get Started
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -37,3 +32,4 @@ function Header() {
 }
 
 export default Header;
+
---
+++ src/components/RotatingText.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+
+function RotatingText() {
+    const texts = [
+        'Family Photos',
+        'Home Cleaning',
+        'Renovation Project',
+        'Car Detailing',
+        'Moving Service',
+        'Landscaping Job',
+        'Painting Project',
+        'Web Design Project',
+        'Therapy Session',
+        'Legal Service',
+        'Chiropractic Visit',
+        'CPA Service',
+        'Financial Planning',
+        'Tutoring Session',
+        'DJ Booking',
+        'Personal Training',
+        'Piano Lesson',
+        'Haircut',
+        'Manicure',
+        'Language Lesson',
+        'Social Media Management',
+        'Interior Design',
+    ];
+    
+    
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        }, 3000); // Change text every 3 seconds
+
+        return () => clearInterval(intervalId);
+    }, [texts.length]);
+
+    return (
+        <div className="rotating-text-wrapper">
+            <div className="rotating-text">
+                {texts[currentIndex]}
+            </div>
+        </div>
+    );
+}
+
+export default RotatingText;
