9516507f114e866511b32bb8fd588eb34a854d3e
19wburnett <wburnet0@byu.edu>
2024-09-04 22:37:58 -0600

    Added second part of homepage, still not finished, mobile needs work.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,7 +5,7 @@ import TestimonialSlider from './Layout/Testimonials/TestimonialSlider';
 import '../App.css';
 import { Link } from 'react-router-dom';
 import videoSrc from '../assets/images/Landing Page Video 2.mp4'; // Import the video
-
+import bidiLogoWhite from '../assets/images/bidi logo white.png';
 
 function Homepage() {
     const [user, setUser] = useState(null);
@@ -24,44 +24,65 @@ function Homepage() {
 
     return (
         <>
-            <header className="masthead-index">
-                <div className="container flex-container">
-                    <div className="text-side">
-                        <div className="text-container">
-                            <div className='homepage-title'>
-                                Bidi is a platform that makes it easy to get bids on 
+            <div className="header-container">
+                <header className="masthead-index">
+                    <div className="container flex-container">
+                        <div className="text-side">
+                            <div className="text-container">
+                                <div className='homepage-bidi-logo'>
+                                    <img src={bidiLogoWhite} alt="Bidi Logo" />
+                                </div>
+                                <div className='homepage-title'>
+                                    A platform that makes it easy to get bids on 
+                                </div>
+                                <div className='rotating-text'><RotatingText /></div>
+                                <div className="search-container">
+                                    {user === null ? (
+                                        <Link to="Signup">
+                                            <button className="search-button">Get Started</button>
+                                        </Link>
+                                    ) : (
+                                        <Link to="/request-categories">
+                                            <button className="search-button">Get Started</button>
+                                        </Link>
+                                    )}
+                                </div>
                             </div>
-                            <div className='rotating-text'><RotatingText /></div>
-                            
                         </div>
                     </div>
+                </header>
+                {/* Wave container for SVG */}
+                <div className="wave-container-top">
+                    <svg className="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+                    <path d="M0,200 Q80,180 160,200 T320,200 T480,200 T640,200 T800,200 T960,200 T1120,200 T1280,200 T1440,200 V320 H0 Z"/></svg>
                 </div>
-            </header>
-            <div className="video-container">
-                {/* iPhone Frame with Video inside */}
-                <div className="iphone-container">
-                    <video src={videoSrc} className="iphone-video" autoPlay loop muted playsInline />
-                </div>
-                <div className='homepage-subtitle'>
-                                You can start getting bids in 3 easy steps:
-                            </div>
-                            <div className='homepage-steps'>
-                                <p>1: Submit a Request</p>
-                                <p>2: Get bids</p>
-                                <p>3: Pick the Bid that Works for You</p>
-                            </div>
-                <div className="search-container">
-                                {user === null ? (
-                                    <Link to="Signup">
-                                        <button className="search-button">Get Started</button>
-                                    </Link>
-                                ) : (
-                                    <Link to="/request-categories">
-                                        <button className="search-button">Get Started</button>
-                                    </Link>
-                                )}
-                            </div>
             </div>
+            <div className="video-container">
+    <div className="iphone-container">
+        <video src={videoSrc} className="iphone-video" autoPlay loop muted playsInline />
+    </div>
+    <div className="text-container-2nd-page">
+        <div className="homepage-subtitle">
+            You can start getting bids in 3 easy steps
+        </div>
+        <div className="homepage-steps">
+            <p>Submit a Request</p>
+            <p>Get bids</p>
+            <p>Pick the Bid that Works for You</p>
+        </div>
+        <div className="try-it-out-container">
+                                    {user === null ? (
+                                        <Link to="Signup">
+                                            <button className="search-button-secondary">Try it out</button>
+                                        </Link>
+                                    ) : (
+                                        <Link to="/request-categories">
+                                            <button className="search-button-secondary">Try it out</button>
+                                        </Link>
+                                    )}
+                                </div>
+    </div>
+</div>
             <div className='testimonials-container'>
                 <div className="text-container">
                     <h2 className="testimonial-header">Don't take our word for it. Take theirs.</h2>
