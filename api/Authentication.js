30517f5080ccc3000f40cb64ca7f3e776f221058
baylorjep <baylorjeppsen@gmail.com>
2024-09-27 02:33:44 +0200

    making live adjustments

--- api/Authentication.js
+++ api/Authentication.js
@@ -1,7 +1,7 @@
 //with connect, 
 
 (async () => {
-    const stripe = require( 'stripe')('sk_test_51Pv13ZF25aBU3RMPjAxWeSf0Cvnp6OI0n5MlmU8dLopD2g5gBDOcD0oRs6RAj56SfF5pVACra3BSjJIRDphUNoJm00KUr0QoqJ');
+    const stripe = require( 'stripe')(process.env.STRIPE_SECRET_KEY);
     const customer = await stripe.customers.retrieve('Cus_HDfWzCQ6UEVtfu', {
     stripeAccount: 'acct_1Ey3h1฿qeQDKpna'
 }) ;
