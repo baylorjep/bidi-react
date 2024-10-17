0772ad06a4507cf2385df7f2de0a7fc5d6cf5a2e
19wburnett <wburnet0@byu.edu>
2024-10-17 00:04:16 -0600

    Fixed mobile buttons for choose account type

--- src/components/Profile/ChooseUserType.js
+++ src/components/Profile/ChooseUserType.js
@@ -13,17 +13,22 @@ function ChooseUserType() {
         <div className="text-center">
             <div className="Sign-Up-Page-Header" style={{ marginTop: '40px' }}>Create Account</div>
             <div className="choose-type-button-container">
-                <button 
+                <div>
+                   <button 
                     onClick={() => handleSelection('individual')} 
                     className="choose-type-button">
                     Hire someone to help me
-                </button>
+                </button> 
+                </div>
+                
                 <div className='choose-type-or'>OR</div>
-                <button 
+                <div><button 
                     onClick={() => handleSelection('business')} 
                     className="choose-type-button">
                     Offer my skills to others
                 </button>
+                </div>
+                
             </div>
         </div>
           
