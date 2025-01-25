e35a8a6c8aabb196b91b0758af982cc12bbb92eb
baylorjep <baylorjeppsen@gmail.com>
2025-01-25 11:50:01 -0700

    email photographers removal

--- src/components/Event/EventSummary.js
+++ src/components/Event/EventSummary.js
@@ -266,27 +266,6 @@ function UploadPictures() {
                     }
                 }
             }
-
-            // Send email notification
-        const emailPayload = {
-            category: eventType, // Use the event type
-        };
-
-        const emailResponse = await fetch('https://bidi-express.vercel.app/send-resend-email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(emailPayload),
-        });
-
-        if (!emailResponse.ok) {
-            const errorDetails = await emailResponse.json();
-            console.error('Failed to send email:', errorDetails);
-            throw new Error('Failed to send email notifications.');
-        }
-
-        console.log('Emails sent successfully!');
             
 
             // Clear localStorage and navigate
