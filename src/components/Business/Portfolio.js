00a1f802e7b91d85db317550d27318e458b002b7
baylorjep <baylorjeppsen@gmail.com>
2025-02-06 14:44:18 -0700

    portfolios

---
+++ src/components/Business/Portfolio.js
@@ -0,0 +1,10 @@
+// this is where you will build in the component for the portfolio page
+
+//1. Update BidDisplay.js (the component used to display a bid to users)(it's in the components/Bid directory ) 
+// so that when users click the business name, they are taken to the business's portfolio page.
+
+//2. Finish creating this component:  Portfolio.js,
+// start with a basic view  that will show the portfolio page from a given business
+
+//3. Update App.js (in the src directory) to handle the routing for the portfolio page.
+// my guess is the best way to do this is by grabbing the associated business ID from the bid and passing it to the portfolio page
