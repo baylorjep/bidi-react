b47ff6c0904c8093d60f0f363531c9c1ad221ef3
JadenGatherum <130808569+JadenGatherum@users.noreply.github.com>
2025-02-08 18:21:37 -0700

    Made a basic profile view

--- src/App.js
+++ src/App.js
@@ -1,181 +1,214 @@
 // App Imports
-import './App.css';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './i18n';
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./i18n";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // Layout Imports
-import Navbar from './components/Layout/Navbar';
-import Footer from './components/Layout/Footer';
-import PrivacyPolicy from './components/Layout/PrivacyPolicy';
-import TermsOfUse from './components/Layout/TermsOfUse';
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import PrivacyPolicy from "./components/Layout/PrivacyPolicy";
+import TermsOfUse from "./components/Layout/TermsOfUse";
 
 // Admin Imports
-import AdminDashboard from './components/admin/AdminDashboard';
+import AdminDashboard from "./components/admin/AdminDashboard";
 
 // Bid Imports
-import SubmitBid from './components/Request/SubmitBid';
-import BidSuccess from './components/Bid/BidSuccess';
-import BidAccepted from './components/Bid/BidAccepted';
+import SubmitBid from "./components/Request/SubmitBid";
+import BidSuccess from "./components/Bid/BidSuccess";
+import BidAccepted from "./components/Bid/BidAccepted";
 
 // Request Imports
-import RequestCategories from './components/Request/RequestCategories';
-import RequestForm from './components/Request/RequestForm';
-import SuccessRequest from './components/Request/SuccessRequest';
-import OpenRequests from './components/Request/OpenRequests';
+import RequestCategories from "./components/Request/RequestCategories";
+import RequestForm from "./components/Request/RequestForm";
+import SuccessRequest from "./components/Request/SuccessRequest";
+import OpenRequests from "./components/Request/OpenRequests";
 
 // New staged request imports
-import MultiStepRequestForm from './components/Request/general requests/MultiStepRequestForm';
+import MultiStepRequestForm from "./components/Request/general requests/MultiStepRequestForm";
 
 // test email imports
-import TestEmail from './components/TestEmail';
+import TestEmail from "./components/TestEmail";
 
 // Event Imports
-import SelectEvent from './components/Event/SelectEvent';
-import EventDetails from './components/Event/EventDetails';
-import EventPhotos from './components/Event/UploadPictures';
-import PersonalDetails from './components/Event/PersonalDetails';
-import EventSummary from './components/Event/EventSummary';
+import SelectEvent from "./components/Event/SelectEvent";
+import EventDetails from "./components/Event/EventDetails";
+import EventPhotos from "./components/Event/UploadPictures";
+import PersonalDetails from "./components/Event/PersonalDetails";
+import EventSummary from "./components/Event/EventSummary";
 
 // Profile Imports
-import Signup from './components/Profile/Signup';
-import SignIn from './components/Profile/SignIn';
-import ChooseUserType from './components/Profile/ChooseUserType';
-import ChoosePricingPlan from './components/Profile/ChoosePricingPlan';
-import SuccessSignup from './components/Profile/SuccessSignup';
-import ResetPassword from './components/Profile/ResetPassword';
-import UpdatePassword from './components/Profile/UpdatePassword';
-import ProfilePage from './components/Profile/Profile';
+import Signup from "./components/Profile/Signup";
+import SignIn from "./components/Profile/SignIn";
+import ChooseUserType from "./components/Profile/ChooseUserType";
+import ChoosePricingPlan from "./components/Profile/ChoosePricingPlan";
+import SuccessSignup from "./components/Profile/SuccessSignup";
+import ResetPassword from "./components/Profile/ResetPassword";
+import UpdatePassword from "./components/Profile/UpdatePassword";
+import ProfilePage from "./components/Profile/Profile";
 
 // Individual Imports
-import MyDashboard from './components/Individual/MyDashboard'
-import MyRequests from './components/Individual/MyRequests';
-import EditRequest from './components/Individual/EditRequest';
+import MyDashboard from "./components/Individual/MyDashboard";
+import MyRequests from "./components/Individual/MyRequests";
+import EditRequest from "./components/Individual/EditRequest";
 
 // Business Imports
-import BusinessDashboard from './components/Business/BusinessDashboard';
-import EditBid from './components/Business/EditBid';
+import BusinessDashboard from "./components/Business/BusinessDashboard";
+import EditBid from "./components/Business/EditBid";
+import Portfolio from "./components/Business/Portfolio";
+
 // Misc Imports
-import Homepage from './components/Homepage';
-import ContactForm from './components/ContactForm';
-import AboutUs from './components/AboutUs';
+import Homepage from "./components/Homepage";
+import ContactForm from "./components/ContactForm";
+import AboutUs from "./components/AboutUs";
 
 // Spanish imports
-import HomepageES from './components/HomepageES'
-import ContactFormES from './components/ContactFormES';
+import HomepageES from "./components/HomepageES";
+import ContactFormES from "./components/ContactFormES";
 
 // Stripe imports
-import Onboarding from './components/Stripe/Onboarding';
-import StripeOnboarding from './components/Stripe/StripeOnboarding';
-import EmbeddedCheckoutForm from './components/Stripe/EmbeddedCheckoutForm';
-import PaymentCancelled from './components/Stripe/PaymentCancelled';
-import SuccessPayment from './components/Stripe/SuccessfulPayment';
-import PaymentStatus from './components/Stripe/PaymentStatus';
+import Onboarding from "./components/Stripe/Onboarding";
+import StripeOnboarding from "./components/Stripe/StripeOnboarding";
+import EmbeddedCheckoutForm from "./components/Stripe/EmbeddedCheckoutForm";
+import PaymentCancelled from "./components/Stripe/PaymentCancelled";
+import SuccessPayment from "./components/Stripe/SuccessfulPayment";
+import PaymentStatus from "./components/Stripe/PaymentStatus";
 
 // ScrollToTop import
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 
 // Add this import
-import BidsPage from './components/Individual/BidsPage'
-import PrivateRoute from './components/PrivateRoute';
+import BidsPage from "./components/Individual/BidsPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
+  const [eventType, setEventType] = useState("");
+  const [eventDetails, setEventDetails] = useState({});
 
-    const [eventType, setEventType] = useState('');
-    const [eventDetails, setEventDetails] = useState({});
-
-    return (
-        <Router>
-            <ScrollToTop />
-            <div className="app-container">
-                <Navbar />
-                <div className='content'>
-                    <Routes>
-                        {/* Layout Routes */}
-                        <Route path="/" element={<Homepage />} />
-                        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-                        <Route path="/terms-of-use" element={<TermsOfUse />} />
-
-                        {/* Bid Routes */}
-                        <Route path="/submit-bid/:requestId" element={<SubmitBid />} />
-                        <Route path="/bid-accepted" element={<BidAccepted />} />
-                        <Route path="/bid-success" element={<BidSuccess />} />
-
-                        {/* Request Routes */}
-                        <Route path="/request-categories" element={<RequestCategories />} />
-                        <Route path="/request" element={<RequestForm />} />
-                        <Route path="/success-request" element={<SuccessRequest />} />
-
-                        {/* New Staged Request Routes */}
-                        <Route path="/request-form" element={<MultiStepRequestForm />} />
-                        <Route path="/success-request" element={<SuccessRequest />} />
-
-                        {/* Event Routes */}
-                        <Route path="/select-event" element={<SelectEvent setEventType={setEventType} />} />
-                        <Route path="/event-details" element={<EventDetails eventType={eventType} setEventDetails={setEventDetails} />} />
-                        <Route path="/event-photos" element={<EventPhotos eventType={eventType} setEventDetails={setEventDetails} />} />
-                        <Route path="/personal-details" element={<PersonalDetails eventType={eventType} setEventDetails={setEventDetails} />} />
-                        <Route path="/event-summary" element={<EventSummary eventType={eventType} eventDetails={eventDetails} />} />
-
-                        {/* Profile Routes */}
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/success-signup" element={<SuccessSignup />} />
-                        <Route path='/choose-pricing-plan' element={<ChoosePricingPlan/>} />
-                        <Route path="/signin" element={<SignIn />} />
-                        <Route path="/request-password-reset" element={<ResetPassword />} />
-                        <Route path="/reset-password" element={<UpdatePassword />} />
-                        <Route path="/createaccount" element={<ChooseUserType />} />
-                        <Route path="/choose-pricing" element={<ChoosePricingPlan />}/>
-                        <Route path="/profile" element={<ProfilePage />} />
-
-                        {/* Individual Routes */}
-                        <Route path="/my-dashboard" element={<MyDashboard />} />
-                        <Route path="/my-requests" element={<MyRequests />} />
-                        <Route path="/edit-request/:type/:id" element={<EditRequest />} />
-                        
-                        {/* Test API Routes */}
-                         <Route path="/test-email" element={<TestEmail />} />
-                        
-                        {/* Business Routes */}
-                        <Route path="/open-requests" element={<OpenRequests />} />
-                        <Route path="/dashboard" element={<BusinessDashboard />} />
-                        <Route path="/edit-bid/:requestId/:bidId" element={<EditBid />} /> {/* Dynamic URL for editing bids */}
-
-                        {/* Misc Routes */}
-                        <Route path="/contact-us" element={<ContactForm />} />
-                        <Route path="/about-us" element={<AboutUs />} />
-
-                        {/* Spanish Routes */}
-                        <Route path="/inicio" element={<HomepageES />} />
-                        <Route path="/contactenos" element={<ContactFormES />} />
-
-                        {/* Stripe Routes */}
-                        <Route path="/onboarding" element={<Onboarding />} />
-                        <Route path="/stripe-setup" element={<StripeOnboarding />} />
-                        <Route path="/checkout" element={<EmbeddedCheckoutForm />} />
-                        <Route path="/payment-cancelled" element={<PaymentCancelled />} />
-                        <Route path="/payment-successful" element={<SuccessPayment />} />
-                        <Route path="/payment-status" element={<PaymentStatus />} />
-
-                        {/* Admin Routes */}
-                        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-
-                        {/* Combined Bids Route */}
-                        <Route path="/bids" element={
-                            <PrivateRoute>
-                                <BidsPage />
-                            </PrivateRoute>
-                        } />
-
-                    </Routes>
-                </div>
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <ScrollToTop />
+      <div className="app-container">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            {/* Layout Routes */}
+            <Route path="/" element={<Homepage />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-use" element={<TermsOfUse />} />
+            {/* Bid Routes */}
+            <Route path="/submit-bid/:requestId" element={<SubmitBid />} />
+            <Route path="/bid-accepted" element={<BidAccepted />} />
+            <Route path="/bid-success" element={<BidSuccess />} />
+            {/* Request Routes */}
+            <Route path="/request-categories" element={<RequestCategories />} />
+            <Route path="/request" element={<RequestForm />} />
+            <Route path="/success-request" element={<SuccessRequest />} />
+            {/* New Staged Request Routes */}
+            <Route path="/request-form" element={<MultiStepRequestForm />} />
+            <Route path="/success-request" element={<SuccessRequest />} />
+            {/* Event Routes */}
+            <Route
+              path="/select-event"
+              element={<SelectEvent setEventType={setEventType} />}
+            />
+            <Route
+              path="/event-details"
+              element={
+                <EventDetails
+                  eventType={eventType}
+                  setEventDetails={setEventDetails}
+                />
+              }
+            />
+            <Route
+              path="/event-photos"
+              element={
+                <EventPhotos
+                  eventType={eventType}
+                  setEventDetails={setEventDetails}
+                />
+              }
+            />
+            <Route
+              path="/personal-details"
+              element={
+                <PersonalDetails
+                  eventType={eventType}
+                  setEventDetails={setEventDetails}
+                />
+              }
+            />
+            <Route
+              path="/event-summary"
+              element={
+                <EventSummary
+                  eventType={eventType}
+                  eventDetails={eventDetails}
+                />
+              }
+            />
+            {/* Profile Routes */}
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/success-signup" element={<SuccessSignup />} />
+            <Route
+              path="/choose-pricing-plan"
+              element={<ChoosePricingPlan />}
+            />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/request-password-reset" element={<ResetPassword />} />
+            <Route path="/reset-password" element={<UpdatePassword />} />
+            <Route path="/createaccount" element={<ChooseUserType />} />
+            <Route path="/choose-pricing" element={<ChoosePricingPlan />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            {/* Individual Routes */}
+            <Route path="/my-dashboard" element={<MyDashboard />} />
+            <Route path="/my-requests" element={<MyRequests />} />
+            <Route path="/edit-request/:type/:id" element={<EditRequest />} />
+            {/* Test API Routes */}
+            <Route path="/test-email" element={<TestEmail />} />
+            {/* Business Routes */}
+            <Route path="/open-requests" element={<OpenRequests />} />
+            <Route path="/dashboard" element={<BusinessDashboard />} />
+            <Route
+              path="/edit-bid/:requestId/:bidId"
+              element={<EditBid />}
+            />{" "}
+            {/* Dynamic URL for editing bids */}
+            <Route path="/portfolio/:businessId" element={<Portfolio />} />
+            {/* Misc Routes */}
+            <Route path="/contact-us" element={<ContactForm />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            {/* Spanish Routes */}
+            <Route path="/inicio" element={<HomepageES />} />
+            <Route path="/contactenos" element={<ContactFormES />} />
+            {/* Stripe Routes */}
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="/stripe-setup" element={<StripeOnboarding />} />
+            <Route path="/checkout" element={<EmbeddedCheckoutForm />} />
+            <Route path="/payment-cancelled" element={<PaymentCancelled />} />
+            <Route path="/payment-successful" element={<SuccessPayment />} />
+            <Route path="/payment-status" element={<PaymentStatus />} />
+            {/* Admin Routes */}
+            <Route path="/admin-dashboard" element={<AdminDashboard />} />
+            {/* Combined Bids Route */}
+            <Route
+              path="/bids"
+              element={
+                <PrivateRoute>
+                  <BidsPage />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Business/Portfolio.js
+++ src/components/Business/Portfolio.js
@@ -1,6 +1,6 @@
 // this is where you will build in the component for the portfolio page
 
-//1. Update BidDisplay.js (the component used to display a bid to users)(it's in the components/Bid directory ) 
+//1. Update BidDisplay.js (the component used to display a bid to users)(it's in the components/Bid directory )
 // so that when users click the business name, they are taken to the business's portfolio page.
 
 //2. Finish creating this component:  Portfolio.js,
@@ -8,3 +8,74 @@
 
 //3. Update App.js (in the src directory) to handle the routing for the portfolio page.
 // my guess is the best way to do this is by grabbing the associated business ID from the bid and passing it to the portfolio page
+
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { supabase } from "../../supabaseClient"; // Import Supabase client
+
+const Portfolio = () => {
+  const { businessId } = useParams(); // Get business ID from URL
+  const [business, setBusiness] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBusinessPortfolio = async () => {
+      const { data, error } = await supabase
+        .from("business_profiles")
+        .select("*")
+        .eq("id", businessId)
+        .single();
+
+      if (error) {
+        setError("Failed to load business portfolio");
+        console.error(error);
+      } else {
+        setBusiness(data);
+      }
+      setLoading(false);
+    };
+
+    fetchBusinessPortfolio();
+  }, [businessId]);
+
+  if (loading) return <p>Loading portfolio...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div className="container">
+      <h1>{business.business_name}</h1>
+      <p>
+        <strong>Category:</strong> {business.business_category}
+      </p>
+      {business.website && (
+        <p>
+          <strong>Website:</strong>{" "}
+          <a href={business.website} target="_blank" rel="noopener noreferrer">
+            {business.website}
+          </a>
+        </p>
+      )}
+      <p>
+        <strong>Description:</strong>{" "}
+        {business.description || "No description available"}
+      </p>
+
+      {/* Display portfolio images if available */}
+      {business.portfolio_images && (
+        <div className="portfolio-gallery">
+          {business.portfolio_images.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="Portfolio"
+              className="portfolio-image"
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Portfolio;
