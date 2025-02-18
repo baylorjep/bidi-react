589bf0a2943535fd7db67a3c49ff1e5291ae94b0
19wburnett <weston.burnett19@gmail.com>
2025-02-18 10:02:53 -0700

    Changed order

--- src/components/UserReviews.js
+++ src/components/UserReviews.js
@@ -15,16 +15,17 @@ import ChandraIcon from '../assets/images/Chandra cropped.png'
 const UserReviews = () => {
   const reviews = [
     {
-      avatar:KaylaIcon,
-      name: "Kayla B.",
-      date: "February 07th, 2025",
-      review:"I came across Bidi for the first time in its early days while planning my wedding and searching for an easier way to request bids from vendors aside from sending hundreds of blind emails. The team over at Bidi won me over by not only saving me precious time, but also with their exceptional customer service. Eager to grow and make the platform more user friendly, they took the recommendations I had to heart, and weeks later, I am beyond impressed with the improvements they've made. It's a thriving platform built truly optimized for the user experience. It's super cool seeing a young company grow so fast, all while filling a huge gap in the wedding industry. Super grateful for Bidi!"
-    },
-    {
       avatar: ChandraIcon,
       name: "Chandra W.",
       date: "Februrary 17th, 2025",
       review: "If you're planning a wedding, I can't recommend Bidi enough! I received offers from multiple vendors within hours, making the process incredibly smooth. It’s like having a personal wedding planner right at your fingertips—fast, easy, and incredibly resourceful. Definitely worth it!!!"
+    },
+    
+    {
+      avatar:KaylaIcon,
+      name: "Kayla B.",
+      date: "February 07th, 2025",
+      review:"I came across Bidi for the first time in its early days while planning my wedding and searching for an easier way to request bids from vendors aside from sending hundreds of blind emails. The team over at Bidi won me over by not only saving me precious time, but also with their exceptional customer service. Eager to grow and make the platform more user friendly, they took the recommendations I had to heart, and weeks later, I am beyond impressed with the improvements they've made. It's a thriving platform built truly optimized for the user experience. It's super cool seeing a young company grow so fast, all while filling a huge gap in the wedding industry. Super grateful for Bidi!"
     },
 
     {
