19d62ab9bc29dd94f377bbdd649ba9413822b25f
19Wburnett <wburnet0@byu.edu>
2025-07-23 07:13:05 -0600

    Testing Email Fix

--- src/components/admin/AdminDashboard.js
+++ src/components/admin/AdminDashboard.js
@@ -451,7 +451,7 @@ function AdminDashboard() {
           </html>
         `;
         try {
-            const response = await fetch('/send-resend-email', {
+            const response = await fetch('/api/send-resend-email', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
